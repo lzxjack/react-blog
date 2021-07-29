88228f7b27ce68b31359cf6f1580c3fb658ea549
Jack <965555169@qq.com>
2021-07-29 10:52:21 +0800

    添加cdn版本号

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -6,8 +6,8 @@ export const blogAdminUrl =
     'https://blog-admin-7gys9jfy3a4d43aa-1304393382.ap-shanghai.app.tcloudbase.com/';
 
 // 评论模块和配置
-export const twikooUrl = 'https://cdn.jsdelivr.net/gh/lzxjack/cdn/js/twikoo.js';
-export const twikooConfigUrl = 'https://cdn.jsdelivr.net/gh/lzxjack/cdn/js/twikoo-config.js';
+export const twikooUrl = 'https://cdn.jsdelivr.net/gh/lzxjack/cdn@1.0.0/js/twikoo.js';
+export const twikooConfigUrl = 'https://cdn.jsdelivr.net/gh/lzxjack/cdn@1.0.0/js/twikoo-config.js';
 
 // 头像
 export const avatarUrl = 'https://jack-img.oss-cn-hangzhou.aliyuncs.com/img/20201204121004.jpg';
@@ -22,5 +22,5 @@ export const time = '2020-12-16 14:00:00';
 export const pageSize = 8;
 
 // wow.js和配置
-export const wowUrl = 'https://cdn.jsdelivr.net/gh/lzxjack/cdn/js/wow.js';
-export const wowConfigUrl = 'https://cdn.jsdelivr.net/gh/lzxjack/cdn/js/wow-config.js';
+export const wowUrl = 'https://cdn.jsdelivr.net/gh/lzxjack/cdn@1.0.0/js/wow.js';
+export const wowConfigUrl = 'https://cdn.jsdelivr.net/gh/lzxjack/cdn@1.0.0/js/wow-config.js';
