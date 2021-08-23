178e531287a0927785f938322d7e47669282fd68
Jack <965555169@qq.com>
2021-08-23 20:15:55 +0800

    卡片添加跳转链接

--- src/components/Blog/Content/BlogData/index.jsx
+++ src/components/Blog/Content/BlogData/index.jsx
@@ -1,30 +1,38 @@
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import './index.css';
 
-const BlogData = props => (
-    <div className="animated bounceInRight">
-        <div className="BlogData-box transparent-box">
-            <div className="blogData common-hover">
-                <div className="type-name">文章</div>
-                <div className="type-num">{props.articleNum}</div>
-            </div>
-            <div className="blogData common-hover">
-                <div className="type-name">分类</div>
-                <div className="type-num">{props.classNum}</div>
-            </div>
-            <div className="blogData common-hover">
-                <div className="type-name">标签</div>
-                <div className="type-num">{props.tagNum}</div>
+const BlogData = props => {
+    const turnArticles = () => props.history.push('/articles');
+    const turnClasses = () => props.history.push('/classes');
+    const turnTags = () => props.history.push('/tags');
+    return (
+        <div className="animated bounceInRight">
+            <div className="BlogData-box transparent-box">
+                <div className="blogData common-hover" onClick={turnArticles}>
+                    <div className="type-name">文章</div>
+                    <div className="type-num">{props.articleNum}</div>
+                </div>
+                <div className="blogData common-hover" onClick={turnClasses}>
+                    <div className="type-name">分类</div>
+                    <div className="type-num">{props.classNum}</div>
+                </div>
+                <div className="blogData common-hover" onClick={turnTags}>
+                    <div className="type-name">标签</div>
+                    <div className="type-num">{props.tagNum}</div>
+                </div>
             </div>
         </div>
-    </div>
-);
+    );
+};
 
-export default connect(
-    state => ({
-        articleNum: state.articles.length,
-        classNum: state.classes.length,
-        tagNum: state.tags.length,
-    }),
-    {}
-)(BlogData);
+export default withRouter(
+    connect(
+        state => ({
+            articleNum: state.articles.length,
+            classNum: state.classes.length,
+            tagNum: state.tags.length,
+        }),
+        {}
+    )(BlogData)
+);
