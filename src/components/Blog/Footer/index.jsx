27c905e5e282306f9062bae29a03e325ab8cfbb8
飞鸟 <965555169@qq.com>
2022-03-04 00:18:55 +0800

    remove webify

--- src/components/Blog/Footer/index.jsx
+++ src/components/Blog/Footer/index.jsx
@@ -1,50 +1,39 @@
 import './index.css';
 
 const Footer = () => {
-    const arr = ['React', 'Redux', 'AntD', 'CloudBase', 'Webify'];
-    return (
-        <footer>
-            <span>
-                个人博客系统
-                <a
-                    href="https://github.com/lzxjack/blog-show"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="icp"
-                >
-                    「源代码」
-                </a>
-            </span>
-            <span>
-                <a
-                    href="https://beian.miit.gov.cn/#/Integrated/index"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="icp"
-                >
-                    浙ICP备2020043821号-1
-                </a>
-            </span>
-            <span>
-                {arr.map((item, index) => (
-                    <span className="site-frame theme-color common-hover" key={index}>
-                        {item}
-                    </span>
-                ))}
-            </span>
-            {/* <span>
-                Powered by
-                <a
-                    href="https://webify.cloudbase.net/"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="icp"
-                >
-                    「CloudBase Webify」
-                </a>
-            </span> */}
-        </footer>
-    );
+  const arr = ['React', 'Redux', 'AntD', 'CloudBase'];
+  return (
+    <footer>
+      <span>
+        个人博客系统
+        <a
+          href='https://github.com/lzxjack/blog-show'
+          target='_blank'
+          rel='noreferrer'
+          className='icp'
+        >
+          「源代码」
+        </a>
+      </span>
+      <span>
+        <a
+          href='https://beian.miit.gov.cn/#/Integrated/index'
+          target='_blank'
+          rel='noreferrer'
+          className='icp'
+        >
+          浙ICP备2020043821号-1
+        </a>
+      </span>
+      <span>
+        {arr.map((item, index) => (
+          <span className='site-frame theme-color common-hover' key={index}>
+            {item}
+          </span>
+        ))}
+      </span>
+    </footer>
+  );
 };
 
 export default Footer;
