eb364494e9c9a6316707fbb9797296f803fb2b50
飞鸟 <965555169@qq.com>
2022-03-10 15:19:23 +0800

    feat: add clockCard

--- src/pages/Home/Aside/ClockCard/index.tsx
+++ src/pages/Home/Aside/ClockCard/index.tsx
@@ -1,9 +1,40 @@
 import React from 'react';
 import Card from '@/components/Card';
+import { useClock } from './useClock';
+import { useInterval } from 'ahooks';
+import classNames from 'classnames';
 import s from './index.scss';
 
 const ClockCard: React.FC = () => {
-  return <Card>ClockCard</Card>;
+  const { hour, minute, second, runPerSecond } = useClock();
+  useInterval(() => {
+    runPerSecond();
+  }, 1000);
+
+  return (
+    <Card className={s.card}>
+      <div className={s.dial}></div>
+      <div className={s.zero}></div>
+      <div className={s.six}></div>
+      <div className={s.three}></div>
+      <div className={s.nine}></div>
+      <div className={s.container}>
+        <div className={s.dot}></div>
+        <div
+          className={s.clockMinuteLine}
+          style={{ transform: `rotateZ(${minute}deg)` }}
+        ></div>
+        <div
+          className={s.clockHourLine}
+          style={{ transform: `rotateZ(${hour}deg)` }}
+        ></div>
+        <div
+          className={s.clockSecondLine}
+          style={{ transform: `rotateZ(${second}deg)` }}
+        ></div>
+      </div>
+    </Card>
+  );
 };
 
 export default ClockCard;
