30a1fc03bac77312a997c3f1db424acac042d64e
飞鸟 <965555169@qq.com>
2022-03-15 23:08:06 +0800

    feat: update msg page

---
+++ src/components/Comment/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import s from './index.scss';
+
+const Comment: React.FC = () => {
+  return <div>Comment</div>;
+};
+
+export default Comment;
--- src/pages/Msg/index.tsx
+++ src/pages/Msg/index.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 
+import Divider from '@/components/Divider';
 import Layout from '@/components/Layout';
 
 import { Title } from '../titleConfig';
 import s from './index.scss';
+import MsgInfo from './MsgInfo';
 
 const Msg: React.FC = () => {
-  return <Layout title={Title.Msg}>Msg</Layout>;
+  return (
+    <Layout title={Title.Msg}>
+      <MsgInfo />
+      <Divider />
+    </Layout>
+  );
 };
 
 export default Msg;
