7afc1cafd66523b7dc3db9cdb41ae7d53f27fb04
飞鸟 <965555169@qq.com>
2022-03-15 21:58:34 +0800

    feat: finish show page

--- src/pages/Show/index.tsx
+++ src/pages/Show/index.tsx
@@ -1,12 +1,55 @@
+import { useRequest } from 'ahooks';
 import React from 'react';
 
 import Layout from '@/components/Layout';
+import { DB } from '@/utils/apis/dbConfig';
+import { getOrderData } from '@/utils/apis/getOrderData';
+import { staleTime } from '@/utils/constant';
 
 import { Title } from '../titleConfig';
 import s from './index.scss';
 
+interface ShowType {
+  _id: string;
+  cover: string;
+  link: string;
+  name: string;
+  descr: string;
+}
+
 const Show: React.FC = () => {
-  return <Layout title={Title.Show}>Show</Layout>;
+  const { data, loading } = useRequest(getOrderData, {
+    defaultParams: [
+      {
+        dbName: DB.Show,
+        sortKey: 'order',
+        isAsc: true
+      }
+    ],
+    retryCount: 3,
+    cacheKey: DB.Show,
+    staleTime
+  });
+
+  return (
+    <Layout title={Title.Show} loading={loading} className={s.showBox}>
+      {data?.data.map((item: ShowType) => (
+        <div
+          key={item._id}
+          style={{ backgroundImage: `url(${item.cover})` }}
+          className={s.item}
+        >
+          <a href={item.link} rel='noreferrer' target='_blank' className={s.link}>
+            <div className={s.title}>
+              <span>{item.name}</span>
+            </div>
+            <div className={s.descr}>{item.descr}</div>
+            <div className={s.mask} />
+          </a>
+        </div>
+      ))}
+    </Layout>
+  );
 };
 
 export default Show;
