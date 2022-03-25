dcd3d3b985cac8155497913bec69ad86f2976d6f
飞鸟 <965555169@qq.com>
2022-03-25 18:46:54 +0800

    feat: update mobile navBar

--- src/pages/Post/Navbar/index.tsx
+++ src/pages/Post/Navbar/index.tsx
@@ -1,12 +1,16 @@
-// import 'markdown-navbar/dist/navbar.css';
-
 import './index.custom.scss';
 
+import { MenuFoldOutlined } from '@ant-design/icons';
+import { useSafeState } from 'ahooks';
+import { Drawer } from 'antd';
+import classNames from 'classnames';
 import MarkNav from 'markdown-navbar';
 import React from 'react';
 import { connect } from 'react-redux';
 
 import { setNavShow } from '@/redux/actions';
+
+import s from './index.scss';
 
 interface Props {
   content?: string;
@@ -14,14 +18,36 @@ interface Props {
 }
 
 const Navbar: React.FC<Props> = ({ content, setNavShow }) => {
+  const [visible, setVisible] = useSafeState(false);
+
   return (
-    <MarkNav
-      className='postNavBar'
-      source={content || ''}
-      headingTopOffset={10}
-      ordered={false}
-      onNavItemClick={() => setNavShow?.(false)}
-    />
+    <>
+      <div className={s.hoverBar} onClick={() => setVisible(true)}>
+        <MenuFoldOutlined />
+      </div>
+      <MarkNav
+        className={classNames('postNavBar', s.navBar)}
+        source={content || ''}
+        headingTopOffset={10}
+        ordered={false}
+        onNavItemClick={() => setNavShow?.(false)}
+      />
+      <Drawer
+        placement='right'
+        onClose={() => setVisible(false)}
+        visible={visible}
+        className={s.drawer}
+        width={340}
+      >
+        <MarkNav
+          className='postNavBar'
+          source={content || ''}
+          headingTopOffset={10}
+          ordered={false}
+          onNavItemClick={() => setNavShow?.(false)}
+        />
+      </Drawer>
+    </>
   );
 };
 
