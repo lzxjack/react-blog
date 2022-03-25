15b9664c46ce58213fc51065efe62d75b9dbfdb4
飞鸟 <965555169@qq.com>
2022-03-25 16:23:39 +0800

    feat: update

--- public/assets/config.js
+++ public/assets/config.js
@@ -1,6 +1,6 @@
 function inMobile() {
   document.getElementsByTagName('html')[0].style.fontSize =
-    (document.documentElement.clientWidth / 450) * 100 + 'px';
+    document.documentElement.clientWidth / 450 + 'px';
 }
 document.addEventListener('DOMContentLoaded', inMobile);
 window.onresize = inMobile;
