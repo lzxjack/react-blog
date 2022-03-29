afba0ccd49b9432fda3a5c47088265ff603b0b7b
飞鸟 <965555169@qq.com>
2022-03-29 21:15:51 +0800

    update

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,15 @@
 const { override, fixBabelImports } = require('customize-cra');
 
+const rewiredMap = () => config => {
+  config.devtool = config.mode === 'development' ? 'eval-cheap-module-source-map' : false;
+  return config;
+};
+
 module.exports = override(
-    fixBabelImports('import', {
-        libraryName: 'antd',
-        libraryDirectory: 'es',
-        style: 'css',
-    })
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+    style: 'css'
+  }),
+  rewiredMap()
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,10 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import allRudecers from './reducers';
+import { createStore } from 'redux';
+import allReducers from './reducers';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-// 暴露store
-export default createStore(allRudecers, composeWithDevTools(applyMiddleware(thunk)));
+const store =
+  process.env.NODE_ENV === 'development'
+    ? createStore(allReducers, composeWithDevTools())
+    : createStore(allReducers);
+
+export default store;
