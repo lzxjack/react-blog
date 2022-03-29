cd3cff682c96e65e6d5e2b1d68d5e5c8d009bbc3
飞鸟 <965555169@qq.com>
2022-03-29 13:56:52 +0800

    feat: change adminUrl

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -7,7 +7,7 @@ export const icp_site = 'https://beian.miit.gov.cn/#/Integrated/index';
 export const icp_no = '浙ICP备2020043821号-1';
 
 export const blogAdminUrl =
-  'https://react-blog-admin-8fo571wf24c87f9-1304393382.ap-shanghai.app.tcloudbase.com/';
+  'https://react-blog-admin-8fo571wf24c87f9-1304393382.tcloudbaseapp.com/';
 
 export const siteTitle = '飞鸟小站';
 
