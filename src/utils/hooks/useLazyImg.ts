e0431bba19d4b8d5defeeb79c1f9c3c8555a638d
zixuan.li <zixuan.li@nio.com>
2022-06-07 20:51:42 +0800

    feat: update useLazyImg

--- src/utils/hooks/useLazyImg.ts
+++ src/utils/hooks/useLazyImg.ts
@@ -4,16 +4,14 @@ import { useEffect, useRef } from 'react';
 export const useLazyImg = (avatar: string, loading: string) => {
   const imgRef = useRef(null);
   // eslint-disable-next-line no-unused-vars
-  const [_, ratio] = useInViewport(imgRef, {
-    threshold: [1]
-  });
+  const [inViewport] = useInViewport(imgRef);
 
   const [imgUrl, setImgUrl] = useSafeState(loading);
 
   useEffect(() => {
-    if (ratio !== 1) return;
+    if (!inViewport) return;
     setImgUrl(avatar);
-  }, [ratio]);
+  }, [inViewport]);
 
   return { imgRef, imgUrl };
 };
