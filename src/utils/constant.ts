a4cdef4510b63afc885962f156ba5794bb224a92
lzxjack <965555169@qq.com>
2023-04-27 16:29:28 +0800

    feat: 修改后台管理系统url

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -6,8 +6,7 @@ export const source_github = 'https://github.com/lzxjack/react-blog';
 export const icp_site = 'https://beian.miit.gov.cn/#/Integrated/index';
 export const icp_no = '浙ICP备2020043821号-1';
 
-export const blogAdminUrl =
-  'https://react-blog-admin-8fo571wf24c87f9-1304393382.tcloudbaseapp.com/';
+export const blogAdminUrl = 'https://admin.lzxjack.top:81';
 
 export const siteTitle = '飞鸟小站';
 
