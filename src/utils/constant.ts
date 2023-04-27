c43a459e5215298f7a825778ddb16ff763faa3c5
lzxjack <965555169@qq.com>
2023-04-27 15:07:43 +0800

    feat: 修改图床

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -20,39 +20,39 @@ export const csdnUrl = 'https://blog.csdn.net/Jack_lzx';
 export const count_id = 'cd045e756100126d005169f014931c65';
 
 // 微信二维码地址
-export const weChatQRCode = 'https://img.lzxjack.top/img/202203302344287.webp';
+export const weChatQRCode = 'https://img.lzxjack.top:99/202203302344287.webp';
 // QQ二维码地址
-export const QQ_QRCode = 'https://img.lzxjack.top/img/202203302344487.webp';
+export const QQ_QRCode = 'https://img.lzxjack.top:99/202203302344487.webp';
 
 // 透明头像
-export const cardUrl = 'https://img.lzxjack.top/img/202203302348298.webp';
+export const cardUrl = 'https://img.lzxjack.top:99/202203302348298.webp';
 
 // loading
-export const smallLoadingUrl = 'https://img.lzxjack.top/img/202203302022741.webp';
+export const smallLoadingUrl = 'https://img.lzxjack.top:99/202203302022741.webp';
 
 // 博客运行起始日
 export const time = '2020-12-16 14:00:00';
 
 // // 博客背景图片
 // export const blogBackGroundImgs = [
-//   'https://img.lzxjack.top/img/20210818111500.jpg',
-//   'https://img.lzxjack.top/img/20210818111501.png',
-//   'https://img.lzxjack.top/img/20210818111502.jpg',
-//   'https://img.lzxjack.top/img/20211126190312.jpg',
-//   'https://img.lzxjack.top/img/202203241558769.jpg',
-//   'https://img.lzxjack.top/img/202203241604408.jpg',
-//   'https://img.lzxjack.top/img/202203241627101.jpg',
-//   'https://img.lzxjack.top/img/202203241627102.jpg',
-//   'https://img.lzxjack.top/img/202203241627103.jpg'
+//   'https://img.lzxjack.top:99/20210818111500.jpg',
+//   'https://img.lzxjack.top:99/20210818111501.png',
+//   'https://img.lzxjack.top:99/20210818111502.jpg',
+//   'https://img.lzxjack.top:99/20211126190312.jpg',
+//   'https://img.lzxjack.top:99/202203241558769.jpg',
+//   'https://img.lzxjack.top:99/202203241604408.jpg',
+//   'https://img.lzxjack.top:99/202203241627101.jpg',
+//   'https://img.lzxjack.top:99/202203241627102.jpg',
+//   'https://img.lzxjack.top:99/202203241627103.jpg'
 // ];
 // // 背景图选择
 // export const imgNum = 7;
 
 // // 与模式相符合的背景图
 // export const modeBg = [
-//   'https://img.lzxjack.top/img/202203241627101.jpg',
-//   'https://img.lzxjack.top/img/202203242228220.jpg',
-//   'https://img.lzxjack.top/img/202203241627103.jpg'
+//   'https://img.lzxjack.top:99/202203241627101.jpg',
+//   'https://img.lzxjack.top:99/202203242228220.jpg',
+//   'https://img.lzxjack.top:99/202203241627103.jpg'
 // ];
 
 // 数据缓存时间
@@ -67,26 +67,26 @@ export const detailPostSize = 10;
 // 个人信息
 export const myName = '飞鸟';
 export const myLink = 'https://lzxjack.top';
-export const myAvatar = 'https://img.lzxjack.top/img/202203302154224.webp';
+export const myAvatar = 'https://img.lzxjack.top:99/202203302154224.webp';
 export const myDescr = '一只平凡的鸟罢了。';
 export const myEmail = '965555169@qq.com';
 export const adminUid = 'dbee9976b3c14448a06f2006a4795cf2';
 export const QQ = '965555169';
 
-export const myAvatar70 = 'https://img.lzxjack.top/img/202203302156259.webp';
+export const myAvatar70 = 'https://img.lzxjack.top:99/202203302156259.webp';
 
 // 默认头像集合（若用户没获取QQ头像，则随机显示此数组中的头像）
 export const defaultCommentAvatarArr = [
-  'https://img.lzxjack.top/img/202203302148474.webp',
-  'https://img.lzxjack.top/img/202203302148475.webp',
-  'https://img.lzxjack.top/img/202203302148476.webp',
-  'https://img.lzxjack.top/img/202203302148477.webp',
-  'https://img.lzxjack.top/img/202203302148478.webp',
-  'https://img.lzxjack.top/img/202203302148479.webp',
-  'https://img.lzxjack.top/img/202203302148480.webp',
-  'https://img.lzxjack.top/img/202203302148481.webp',
-  'https://img.lzxjack.top/img/202203302148482.webp',
-  'https://img.lzxjack.top/img/202203302148483.webp'
+  'https://img.lzxjack.top:99/202203302148474.webp',
+  'https://img.lzxjack.top:99/202203302148475.webp',
+  'https://img.lzxjack.top:99/202203302148476.webp',
+  'https://img.lzxjack.top:99/202203302148477.webp',
+  'https://img.lzxjack.top:99/202203302148478.webp',
+  'https://img.lzxjack.top:99/202203302148479.webp',
+  'https://img.lzxjack.top:99/202203302148480.webp',
+  'https://img.lzxjack.top:99/202203302148481.webp',
+  'https://img.lzxjack.top:99/202203302148482.webp',
+  'https://img.lzxjack.top:99/202203302148483.webp'
 ];
 
 export const avatarArrLen = defaultCommentAvatarArr.length;
