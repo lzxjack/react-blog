8aa2221371d4d7de966c0a11ead67d6b01e41924
Jack <965555169@qq.com>
2023-07-25 23:10:09 +0800

    refactor: 修改markdown组件

--- src/components/MarkDown/index.tsx
+++ src/components/MarkDown/index.tsx
@@ -1,6 +1,5 @@
 import './hljs.custom.scss';
 
-import { useMount } from 'ahooks';
 import classNames from 'classnames';
 import hljs from 'highlight.js';
 import marked from 'marked';
@@ -13,20 +12,18 @@ interface Props {
   className?: string;
 }
 
-const MarkDown: React.FC<Props> = ({ content, className }) => {
-  useMount(() => {
-    hljs.configure({
-      classPrefix: 'hljs-',
-      languages: ['CSS', 'HTML', 'JavaScript', 'TypeScript', 'Markdown']
-    });
-    marked.setOptions({
-      renderer: new marked.Renderer(),
-      highlight: code => hljs.highlightAuto(code).value,
-      gfm: true, // 默认为true。 允许 Git Hub标准的markdown.
-      breaks: true // 默认为false。 允许回车换行。该选项要求 gfm 为true。
-    });
-  });
+hljs.configure({
+  classPrefix: 'hljs-',
+  languages: ['CSS', 'HTML', 'JavaScript', 'TypeScript', 'Markdown']
+});
+marked.setOptions({
+  renderer: new marked.Renderer(),
+  highlight: code => hljs.highlightAuto(code).value,
+  gfm: true, // 默认为true。 允许 Git Hub标准的markdown.
+  breaks: true // 默认为false。 允许回车换行。该选项要求 gfm 为true。
+});
 
+const MarkDown: React.FC<Props> = ({ content, className }) => {
   return (
     <div
       className={classNames(s.marked, className)}
